7736498363aa698744be2d9ed90ff62343c3a70e
unknown <robert@sagitario.gtep.civ.puc-rio.br>
2013-09-16 08:52:23 -0300

    Melhoria no Zipar.bat para geracao do .zip para entrega do trabalho.

--- Zipar.bat
+++ Zipar.bat
@@ -1,58 +1,61 @@
 
 rmdir /s /q entrega
 
-mkdir entrega
-mkdir entrega\ferramentas
+mkdir deploy
+mkdir deploy\ferramentas
 
-mkdir entrega\TRAB1-1
-mkdir entrega\TRAB1-1\src
-mkdir entrega\TRAB1-1\scripts
-mkdir entrega\TRAB1-1\lib
-mkdir entrega\TRAB1-1\src
-mkdir entrega\TRAB1-1\batches
-mkdir entrega\TRAB1-1\executavel
-mkdir entrega\TRAB1-1\src
-mkdir entrega\TRAB1-1\src\definicao
-mkdir entrega\TRAB1-1\src\implementacao
+mkdir deploy\TRAB1-1
+mkdir deploy\TRAB1-1\src
+mkdir deploy\TRAB1-1\scripts
+mkdir deploy\TRAB1-1\lib
+mkdir deploy\TRAB1-1\src
+mkdir deploy\TRAB1-1\batches
+mkdir deploy\TRAB1-1\executavel
+mkdir deploy\TRAB1-1\src
+mkdir deploy\TRAB1-1\src\definicao
+mkdir deploy\TRAB1-1\src\implementacao
 
-mkdir entrega\TRAB1-2
-mkdir entrega\TRAB1-2\src
-mkdir entrega\TRAB1-2\scripts
-mkdir entrega\TRAB1-2\lib
-mkdir entrega\TRAB1-2\src
-mkdir entrega\TRAB1-2\batches
-mkdir entrega\TRAB1-2\executavel
-mkdir entrega\TRAB1-2\src
-mkdir entrega\TRAB1-2\src\definicao
-mkdir entrega\TRAB1-2\src\implementacao
+mkdir deploy\TRAB1-2
+mkdir deploy\TRAB1-2\src
+mkdir deploy\TRAB1-2\scripts
+mkdir deploy\TRAB1-2\lib
+mkdir deploy\TRAB1-2\src
+mkdir deploy\TRAB1-2\batches
+mkdir deploy\TRAB1-2\executavel
+mkdir deploy\TRAB1-2\src
+mkdir deploy\TRAB1-2\src\definicao
+mkdir deploy\TRAB1-2\src\implementacao
 
-copy ferramentas\ entrega\ferramentas
-copy Debug\lista.exe entrega\TRAB1-1\executavel
-rename entrega\TRAB1-1\executavel\lista.exe TRAB1-1.exe
-copy src\ArcaboucoTeste.lib entrega\TRAB1-1\lib
-copy src\TesteLista.script entrega\TRAB1-1\scripts
-copy src\LISTA.H entrega\TRAB1-1\src\definicao
-copy src\LISTA.C entrega\TRAB1-1\src\implementacao
-copy src\TESTLIS.C entrega\TRAB1-1\src\implementacao
-copy batches\TestaLista.bat entrega\TRAB1-1\batches
-copy batches\LimpaTudo.bat entrega\TRAB1-1\batches
+copy ferramentas\compilebanner.exe deploy\ferramentas
+copy ferramentas\exbestat.exe deploy\ferramentas
+copy Debug\lista.exe deploy\TRAB1-1\executavel
+rename deploy\TRAB1-1\executavel\lista.exe TRAB1-1.exe
+copy src\ArcaboucoTeste.lib deploy\TRAB1-1\lib
+copy src\TesteLista.script deploy\TRAB1-1\scripts
+copy src\LISTA.H deploy\TRAB1-1\src\definicao
+copy src\LISTA.C deploy\TRAB1-1\src\implementacao
+copy src\TESTLIS.C deploy\TRAB1-1\src\implementacao
+copy batches\TestaLista.bat deploy\TRAB1-1\batches
+copy batches\LimpaTudo.bat deploy\TRAB1-1\batches
 
-copy Debug\matriz.exe entrega\TRAB1-2\executavel
-rename entrega\TRAB1-2\executavel\matriz.exe TRAB1-2.exe
-copy src\ArcaboucoTeste.lib entrega\TRAB1-2\lib
-copy src\lista.lib entrega\TRAB1-2\lib
-copy src\TesteMatriz.script entrega\TRAB1-2\scripts
-copy src\MATRIZ.H entrega\TRAB1-2\src\definicao
-copy src\MATRIZ.C entrega\TRAB1-2\src\implementacao
-copy src\TESTMATRIZ.C entrega\TRAB1-2\src\implementacao
-copy batches\TestaMatriz.bat entrega\TRAB1-2\batches
-copy batches\LimpaTudo.bat entrega\TRAB1-2\batches
+copy Debug\matriz.exe deploy\TRAB1-2\executavel
+rename deploy\TRAB1-2\executavel\matriz.exe TRAB1-2.exe
+copy src\ArcaboucoTeste.lib deploy\TRAB1-2\lib
+copy src\lista.lib deploy\TRAB1-2\lib
+copy src\TesteMatriz.script deploy\TRAB1-2\scripts
+copy src\MATRIZ.H deploy\TRAB1-2\src\definicao
+copy src\MATRIZ.C deploy\TRAB1-2\src\implementacao
+copy src\TESTMATRIZ.C deploy\TRAB1-2\src\implementacao
+copy batches\TestaMatriz.bat deploy\TRAB1-2\batches
+copy batches\LimpaTudo.bat deploy\TRAB1-2\batches
 
-copy batches\TestaTudo.bat entrega\
-copy batches\LimpaTudo.bat entrega\
+copy batches\TestaTudo.bat deploy\
+copy batches\LimpaTudo.bat deploy\
 
-pkzipc entrega -add=up 
+del deploy.zip
 
+ferramentas\7za a -tzip deploy.zip deploy
 
+rmdir /s /q deploy
 
 
--- batches/TestaTudo.bat
+++ batches/TestaTudo.bat
@@ -4,9 +4,9 @@ mkdir resultadosDosTestes
 
 del estatisticas.estat
 
-TRAB1-1\executavel\TRAB1-1      /sTRAB1-1\scripts\TesteLista.script     /lresultadosDosTestes\TesteLista.log     /aresultadosDosTestes\estatisticas
+TRAB1-1\executavel\TRAB1-1      /sTRAB1-1\scripts\TesteLista.script      /lresultadosDosTestes\TesteLista.log       /aresultadosDosTestes\estatisticas
 
-TRAB1-2\executavel\TRAB1-2      /sTRAB1-1\scripts\TesteMatriz.script     /lresultadosDosTestes\TesteMatriz.log     /aresultadosDosTestes\estatisticas
+TRAB1-2\executavel\TRAB1-2      /sTRAB1-2\scripts\TesteMatriz.script     /lresultadosDosTestes\TesteMatriz.log     /aresultadosDosTestes\estatisticas
 
 ferramentas\CompileBanner     /c"Estatisticas acumuladas"
-ferramentas\exbestat /eresultadosDosTestes\estatisticas
+ferramentas\exbestat          /eresultadosDosTestes\estatisticas
