a0423d8107b5276ffa06390a232989b6321e8947
Hugo Roque <hugolnx@gmail.com>
2013-09-16 22:06:30 -0300

    faltou os .bat :P

---
+++ ferramentas/exe2txt.bat
@@ -0,0 +1,3 @@
+rename %1%\ferramentas\*.exe *.exe.txt
+rename %1%\TRAB1-1\executavel\*.EXE *.EXE.TXT
+rename %1%\TRAB1-2\executavel\*.EXE *.EXE.TXT
---
+++ ferramentas/txt2exe.bat
@@ -0,0 +1,3 @@
+rename %1%\ferramentas\*.txt *.
+rename %1%\TRAB1-1\executavel\*.TXT *.
+rename %1%\TRAB1-2\executavel\*.TXT *.
